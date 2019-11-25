316813471e5f11f55224fdf05b359dff9bce798b
mateusfg7 <mateusfg77@gmail.com>
2019-11-25 17:06:06 -0300

    fix file write

--- dados/__init__.py
+++ dados/__init__.py
@@ -2,7 +2,7 @@ def validar_dados(caracteristicas, animal):
     resultado = input("[s/n] A resposta esta correta? ")
 
     if resultado == "s":
-        lista = "{},{},{},{},{},{},{},{}".format(
+        lista = "\n{},{},{},{},{},{},{},{}".format(
             caracteristicas[0],
             caracteristicas[1],
             caracteristicas[2],
@@ -12,5 +12,6 @@ def validar_dados(caracteristicas, animal):
             caracteristicas[6],
             animal[0]
         )
-        arquivo = open("dados.csv", "w")
-        arquivo.writelines(lista)
+        arquivo = open("dados.csv", "a")
+        arquivo.write(lista)
+        arquivo.close()
