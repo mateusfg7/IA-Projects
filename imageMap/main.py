38d6f340dc934f4bf0bdc962a8707b3785e01384
mateusfg7 <mateusfelipefg77@gmail.com>
2020-01-04 12:18:41 -0300

    i don't remember what i modified :(, but i be commiting

--- imageMap/main.py
+++ imageMap/main.py
@@ -2,9 +2,6 @@ from PIL import Image
 
 imagem = Image.open('image.jpg')
 
-pixels = list(imagem.getdata())
-
-pixels = str(pixels).replace('251', '1').replace('249', '1').replace('248', "1").replace('250', '1').replace('252', '1')
-pixels = pixels.replace('1','')
+pixels = imagem.getdata()
 
 print(pixels)
