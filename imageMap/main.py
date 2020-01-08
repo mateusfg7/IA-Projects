ccefdfa1917c93e1af34766f77d14b3d3d5c20fd
mateusfg7 <mateusfelipefg77@gmail.com>
2020-01-08 09:55:37 -0300

    acabei o algoritimo de identificar imagens, com catos e cachorros, fase 2: dominar o mundo hehehe

--- imageMap/main.py
+++ imageMap/main.py
@@ -1,7 +1,32 @@
+from sklearn.svm import LinearSVC
+modelo = LinearSVC()
+
 from PIL import Image
 
-imagem = Image.open('image.jpg')
+quadrado = Image.open('quadrado.jpeg')
+circulo = Image.open('circulo.jpeg')
 
-pixels = imagem.getdata()
+pixelsQuadrado = list(quadrado.getdata())
+pixelsCirculo = list(circulo.getdata())
 
-print(pixels)
+quadradoImage = []
+circuloImage = []
+
+for pixel in pixelsQuadrado:
+    for value in pixel:
+        quadradoImage.append(value)
+
+for pixel in pixelsCirculo:
+    for value in pixel:
+        circuloImage.append(value)
+
+
+treino_x = [quadradoImage, circuloImage]
+treino_y = [1, 2]
+
+modelo.fit(treino_x, treino_y)
+
+
+# 1 > CACHORRO
+# 2 > GATO
+print(modelo.predict(treino_x))
